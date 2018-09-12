720eb388299024f507a0a496f0cb3ca2c1e964cb
Diana Voyanets <diana.voyanets@gmail.com>
2018-09-12 09:21:03 +0300

    adding checkbox views in form

--- sources/views/settings.js
+++ sources/views/settings.js
@@ -3,20 +3,46 @@ import {employeesCollection} from "models/employeesCollection";
 
 export default class Settings extends JetView {
 	config() {
-		
 		var SettingsForm = {
 			view: "form",
 			width: 400,
 			localId: "filesUploaderForm",
 			elements:[
-				{ id:"ContactID",name: "ContactID",view:"combo",labelWidth:115, label:"Employee Name",options: { body:{template:"#FirstName# #LastName#",data:employeesCollection}} },
-				{ name: "name",view:"uploader",link: "mylist",value:"Attach File", upload:"http://localhost:3001/formdoUpload"},
-				{ view:"list",  id:"mylist", type:"uploader",autoheight:true, borderless:true},
+				{cols: [
+					{ view: "checkbox",
+						localId:"nameCheckbox",
+						labelWidth:125,
+						label: "Employee Name:",
+						click: () => {
+							if(this.$$("nameCheckbox").data.value === "1") {
+								this.$$("ContactID").show();
+							} else {
+								this.$$("ContactID").hide();
+							}
+						}},
+					{ view: "checkbox",
+						localId: "emailCheckbox",
+						label: "Employee Email",
+						labelWidth:125,
+						click: () => {
+							if (this.$$("emailCheckbox").data.value === "1") {
+								this.$$("Email").show();
+							} else {
+								this.$$("Email").hide();
+							}
+						}},
+				]},
+				{ view:"combo",id:"ContactID",name: "ContactID",labelWidth:125, label:"Employee Name:",options: { body:{template:"#FirstName# #LastName#",data:employeesCollection}},validate:webix.rules.isNotEmpty,invalidMessage: "First Name can not be empty",hidden:true},
+				{ view:"combo",id:"Email",name: "Email",labelWidth:125, label:"Employee Email:",options: { body:{template:"#Email#",data:employeesCollection}},validate:webix.rules.isNotEmpty,invalidMessage: "Email can not be empty",hidden:true},
+				{ view:"uploader",name: "name",link: "mylist",value:"Attach File", upload:"http://localhost:3001/formdoUpload"},
+				{ view:"list", id:"mylist", type:"uploader",autoheight:true, borderless:true},
 				{ view:"button", value:"Save",
 					click: () => {
-						let data = this.$$("filesUploaderForm").getValues();
-						this.$$("filesDatatable").add(data);
-						this.$$("filesUploaderForm").clear();
+						if (this.$$("filesUploaderForm").validate()) {
+							let data = this.$$("filesUploaderForm").getValues();
+							this.$$("filesDatatable").add(data);
+							this.$$("filesUploaderForm").clear();
+						}
 					}
 				},
 			]
@@ -24,10 +50,13 @@ export default class Settings extends JetView {
         
 		var FilesEmployes = {
 			view:"datatable",
+			scroll:false,
+			width: 800,
 			localId: "filesDatatable",
 			columns: [
-				{id:"ContactID",name:"ContactID",header: "Employee Name",width: 200,options:employeesCollection},
-				{id:"name",header: "File Name"},
+				{ id: "ContactID",name:"ContactID",header: "Employee Name",width: 200,options:employeesCollection},
+				{ id: "Email",header: "Email",width: 300,options:employeesCollection},
+				{ id: "name",header: "File Name",width: 300,fillspace: true},
 			]
 		};
         
@@ -37,8 +66,8 @@ export default class Settings extends JetView {
 					SettingsForm,
 					FilesEmployes,
 					{view: "spacer"}
-			]},
-				{view: "spacer"},
+				]},
+			{view: "spacer"},
 			]};
 	}
 }
